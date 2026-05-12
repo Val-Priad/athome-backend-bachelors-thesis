b2ce38cb34b85d08ffcf8d5e211416d25873414b
Val-Priad <valeriipriadchenko@gmail.com>
2026-05-12 18:06:51 +0200

    feat(domain): add apartment layout enum

---
+++ src/domain/estate/enums/apartment_enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class ApartmentLayout(Enum):
+    studio_1 = "1 studio"
+    one_plus_one = "1+1"
+    studio_2 = "2 studio"
+    two_plus_one = "2+1"
+    studio_3 = "3 studio"
+    three_plus_one = "3+1"
+    studio_4 = "4 studio"
+    four_plus_one = "4+1"
+    studio_5 = "5 studio"
+    five_plus_one = "5+1"
+    six_or_more = "6 or more"
+    atypical = "Atypical"
+    room = "Room"
